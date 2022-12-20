0ef368553d134bdbac85cfe4b8968a6ed56e41ac
Maksim1903 <dovgusha.m@gmail.com>
2022-12-20 13:29:52 +0100

    ,

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,29 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('click', clickOnForm);
+
+function clickOnForm(e) {
+  e.preventDefault();
+  const elements = ({ delay, amount, step } = e.currentTarget);
+  const delayTime = delay.value;
 }
