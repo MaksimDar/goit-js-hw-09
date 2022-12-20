796f252e1236c1ebcd0a64832e6db6aba86783de
MaksimDar <109236938+MaksimDar@users.noreply.github.com>
2022-12-20 13:31:56 +0000

    Deploying to gh-pages from @ MaksimDar/goit-js-hw-09.@e8c68150eeb93237ea9a36ab84cbf6de79661e6c 🚀

--- 01-color-switcher.03ce5d57.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");function n(){r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}timerId=null,t.addEventListener("click",(function(t){n(),timerId=setInterval(n,1e3),t.currentTarget.disabled=!0})),e.addEventListener("click",(function(e){clearInterval(timerId),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.03ce5d57.js.map
