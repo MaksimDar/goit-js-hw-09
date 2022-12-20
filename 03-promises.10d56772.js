dd169a2937b424318f8d5e9946d8e0be317bfc08
MaksimDar <109236938+MaksimDar@users.noreply.github.com>
2022-12-20 12:30:27 +0000

    Deploying to gh-pages from @ MaksimDar/goit-js-hw-09.@0ef368553d134bdbac85cfe4b8968a6ed56e41ac 🚀

---
+++ 03-promises.10d56772.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("click",(function(e){e.preventDefault();({delay:delay,amount:amount,step:step}=e.currentTarget),delay.value}));
+//# sourceMappingURL=03-promises.10d56772.js.map
---
+++ 03-promises.f07f6ed0.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("click",(function(e){var t;e.preventDefault(),t=e.currentTarget,delay=t.delay,amount=t.amount,step=t.step,delay.value}));
+//# sourceMappingURL=03-promises.f07f6ed0.js.map
